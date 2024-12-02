bf74229adf981e5afae72056f692c424961339ae
JarolyP <jobs13az@gmail.com>
2024-12-01 22:10:58 -0400

    subiendo este disparate

--- sidebar.php
+++ sidebar.php
@@ -1,47 +1,47 @@
-  <aside class="main-sidebar sidebar-dark-success elevation-4">
+<aside class="main-sidebar sidebar-dark-success elevation-4">
     <div class="dropdown">
-      <a href="./" class="brand-link">
-        <?php if ($_SESSION['login_type'] == 1): ?>
-          <h3 class="text-center p-0 m-0"><b>SIS-GPT</b></h3>
+   	<a href="./" class="brand-link">
+        <?php if($_SESSION['login_type'] == 1): ?>
+        <h3 class="text-center p-0 m-0"><b>SIS-GPT</b></h3>
         <?php else: ?>
-          <h3 class="text-center p-0 m-0"><b>SIS-GPT</b></h3>
+        <h3 class="text-center p-0 m-0"><b>SIS-GPT</b></h3>
         <?php endif; ?>
 
-      </a>
-
+    </a>
+      
     </div>
     <div class="sidebar pb-4 mb-4">
       <nav class="mt-2">
         <ul class="nav nav-pills nav-sidebar flex-column nav-flat" data-widget="treeview" role="menu" data-accordion="false">
-          <?php if ($_SESSION['login_type'] == 1): ?>
+          <?php if($_SESSION['login_type'] == 1): ?>
             <li class="nav-item">
-              <a href="./index.php?page=user_list" class="nav-link nav-user_list">
-                <i class="fas fa-users nav-icon"></i>
-                <p>Usuarios</p>
-              </a>
-            </li>
-          <?php endif; ?>
+                <a href="./index.php?page=user_list" class="nav-link nav-user_list">
+                  <i class="fas fa-users nav-icon"></i>
+                  <p>Usuarios</p>
+                </a>
+          </li>
+        <?php endif; ?>
         </ul>
       </nav>
     </div>
   </aside>
   <script>
-    $(document).ready(function() {
+  	$(document).ready(function(){
       var page = '<?php echo isset($_GET['page']) ? $_GET['page'] : 'home' ?>';
-      var s = '<?php echo isset($_GET['s']) ? $_GET['s'] : '' ?>';
-      if (s != '')
-        page = page + '_' + s;
-      if ($('.nav-link.nav-' + page).length > 0) {
-        $('.nav-link.nav-' + page).addClass('active')
-        if ($('.nav-link.nav-' + page).hasClass('tree-item') == true) {
-          $('.nav-link.nav-' + page).closest('.nav-treeview').siblings('a').addClass('active')
-          $('.nav-link.nav-' + page).closest('.nav-treeview').parent().addClass('menu-open')
-        }
-        if ($('.nav-link.nav-' + page).hasClass('nav-is-tree') == true) {
-          $('.nav-link.nav-' + page).parent().addClass('menu-open')
+  		var s = '<?php echo isset($_GET['s']) ? $_GET['s'] : '' ?>';
+      if(s!='')
+        page = page+'_'+s;
+  		if($('.nav-link.nav-'+page).length > 0){
+             $('.nav-link.nav-'+page).addClass('active')
+  			if($('.nav-link.nav-'+page).hasClass('tree-item') == true){
+            $('.nav-link.nav-'+page).closest('.nav-treeview').siblings('a').addClass('active')
+  				$('.nav-link.nav-'+page).closest('.nav-treeview').parent().addClass('menu-open')
+  			}
+        if($('.nav-link.nav-'+page).hasClass('nav-is-tree') == true){
+          $('.nav-link.nav-'+page).parent().addClass('menu-open')
         }
 
-      }
-
-    })
+  		}
+     
+  	})
   </script>
